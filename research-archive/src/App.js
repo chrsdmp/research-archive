159ce2539ae8933a31c7fc108d243238fa819784
benedict1117 <neftalycareg2@gmail.com>
2026-05-12 19:52:20 +0800

    first commit

--- research-archive/src/App.js
+++ research-archive/src/App.js
@@ -1,7 +1,34 @@
+import './App.css';
+import logo from './logo.png';
+import acadexia from './ACADEXIA.png';
 
 function App() {
   return (
-    <h2>Hello World!</h2>
+    <div>
+
+      {/* article part yung dalwang logo sa kaliwa ng search bar*/}
+
+      <nav className="article-navbar">
+
+        
+        <div className="article-brand">
+          <img src={logo} alt="Acadexia Icon" className="article-logo-icon" />
+          <img src={acadexia} alt="Acadexia" className="article-logo-wordmark" />
+        </div>
+
+      </nav>
+      
+      {/* homepage part yung malaking acadexia sa gitna tas maliit na logo sa taas sa gitna din*/}
+
+      <nav className="navbar">
+        <img src={logo} alt="Acadexia Icon" className="logo-icon" />
+      </nav>
+
+      <div className="hero">
+        <img src={acadexia} alt="Acadexia" className="logo-wordmark" />
+      </div>
+
+    </div>
   );
 }
 
