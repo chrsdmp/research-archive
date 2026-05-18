33e68a891ad476373837fcabaa4948da7010eb29
Stephanie Nanoy <nanoysteppenaverde@gmail.com>
2026-05-18 22:34:23 +0800

    add
    sa components

---
+++ research-archive/src/components/landing_page.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="container">
+            <div className="overlay">
+
+                {/* Top Navigation */}
+                <div className="top-left">
+                    <button className="nav-btn">✩ My Profile</button>
+                    <button className="nav-btn">✩ My Library</button>
+                </div>
+
+                {/* Logo */}
+                <div className="logo-box">
+                    <img
+                        src="/Logo.svg"
+                        alt="logo"
+                        className="logo"
+                    />
+                </div>
+
+                {/* Title */}
+                <div className="title-box">
+                    <h1>ACADEXIA</h1>
+                </div>
+
+                {/* Search Bar */}
+                <div className="search-container">
+                    <span className="search-icon">🔍</span>
+
+                    <input
+                        type="text"
+                        placeholder="Search my library..."
+                        className="search-input"
+                    />
+                </div>
+
+                {/* Subtitle */}
+                <div className="subtitle-box">
+                    <p>
+                        Elevating the pursuit of truth into the
+                        <br />
+                        practice of wisdom.
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default App;
